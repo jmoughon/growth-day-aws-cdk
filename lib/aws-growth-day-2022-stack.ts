0844d1f8d2207630361e07823295aa4731f0ffa9
James Moughon <james.moughon@praxent.com>
2022-04-21 14:41:26 -0500

    init

--- lib/aws-growth-day-2022-stack.ts
+++ lib/aws-growth-day-2022-stack.ts
@@ -1,16 +1,127 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  App,
+  CfnOutput,
+  Duration,
+  Stack,
+  StackProps,
+  RemovalPolicy,
+  aws_s3 as s3,
+  aws_ec2 as ec2,
+  aws_cloudfront as cloudfront,
+  aws_iam as iam,
+  aws_cloudfront_origins as cloudfront_origins,
+} from "aws-cdk-lib";
 
 export class AwsGrowthDay2022Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
-
     // The code that defines your stack goes here
+    // CHANGE: We have created the vpc object from the Vpc class.
+    new ec2.Vpc(this, "AwsGrowthDay2022Vpc", {
+      // CHANGE: this is where we define how many AZs to use
+      maxAzs: 2,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsGrowthDay2022Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+      // CHANGE: We define a single subnet configuration per AZ.
+      subnetConfiguration: [
+        {
+          // CHANGE: this is it's CIDR mask so 255.255.255.0
+          cidrMask: 24,
+
+          // CHANGE: a name for each of these subnets
+          name: "public-subnet",
+
+          // CHANGE: and the subnet type to be used - here we will have
+          // a public subnet. There are other options available here.
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    //const zone = route53.HostedZone.fromLookup(this, 'Zone', { domainName: props.domainName });
+    //const siteDomain = props.siteSubDomain + '.' + props.domainName;
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
+      this,
+      "cloudfront-OAI",
+      {
+        comment: `OAI for ${id}`,
+      }
+    );
+    //new CfnOutput(this, "Site", { value: "https://" + siteDomain });
+
+    // Content bucket
+    const siteBucket = new s3.Bucket(this, "SiteBucket", {
+      bucketName: "awsgrowthday2022",
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+
+      /**
+       * The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+       * the new bucket, and it will remain in your account until manually deleted. By setting the policy to
+       * DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
+       */
+      removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+
+      /**
+       * For sample purposes only, if you create an S3 bucket then populate it, stack destruction fails.  This
+       * setting will enable full cleanup of the demo.
+       */
+      autoDeleteObjects: true, // NOT recommended for production code
+    });
+
+    // Grant access to cloudfront
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [siteBucket.arnForObjects("*")],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+    new CfnOutput(this, "Bucket", { value: siteBucket.bucketName });
+
+    // TLS certificate
+    // const certificate = new acm.DnsValidatedCertificate(this, 'SiteCertificate', {
+    //   domainName: siteDomain,
+    //   hostedZone: zone,
+    //   region: 'us-east-1', // Cloudfront only checks this region for certificates.
+    // });
+    // new CfnOutput(this, 'Certificate', { value: certificate.certificateArn });
+
+    // CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, "SiteDistribution", {
+      //certificate: certificate,
+      defaultRootObject: "index.html",
+      //domainNames: [siteDomain],
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 403,
+          responsePagePath: "/error.html",
+          ttl: Duration.minutes(30),
+        },
+      ],
+      defaultBehavior: {
+        origin: new cloudfront_origins.S3Origin(siteBucket, {
+          originAccessIdentity: cloudfrontOAI,
+        }),
+        compress: true,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+    new CfnOutput(this, "DistributionId", {
+      value: distribution.distributionId,
+    });
+
+    // // Route53 alias record for the CloudFront distribution
+    // new route53.ARecord(this, 'SiteAliasRecord', {
+    //   recordName: siteDomain,
+    //   target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    //   zone
     // });
   }
 }

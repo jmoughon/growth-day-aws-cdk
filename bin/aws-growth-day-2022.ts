86666e841b9bc5bf45e3e151fbb0e49cc05656b8
James Moughon <james.moughon@praxent.com>
2022-04-21 14:48:05 -0500

    update import

--- bin/aws-growth-day-2022.ts
+++ bin/aws-growth-day-2022.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
+import { App } from "aws-cdk-lib";
 import { AwsGrowthDay2022Stack } from "../lib/aws-growth-day-2022-stack";
 
-const app = new cdk.App();
+const app = new App();
 new AwsGrowthDay2022Stack(app, "AwsGrowthDay2022Stack", {});
